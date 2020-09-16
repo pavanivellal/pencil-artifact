cbfd82107366486e53b545bc779ea47a016f1371
Pavani Vellal <vellalpavani@gmail.com>
2020-09-16 04:50:17 -0700

    test cases for cart value calculation included

--- src/pencil_artifact/helper/price.clj
+++ src/pencil_artifact/helper/price.clj
@@ -43,6 +43,88 @@
                                   :final-price 0
                                   :offers (:offers volume-offers)}))))
 
+(defn calculate-cart-total
+  "Calculate total amount of cart given list of items"
+  [items]
+  (reduce (fn [sum item]
+            (let [best-price (get-best-price (:id item) (:offer-date item) (:qty item))]
+              (+ sum (:final-price best-price)))) 0 items))
+
+(comment
+
+  ;; Test 1 -------------------
+  ;On 10/1/2021, add these items: 8 Cookies, 4 Cheesecakes. Verify the price is $30.
+  ;
+  ;-> 8 cookies (item code 3) =  6
+  ;-> 4 cheesecakes (item code 2) = 24
+  ;TOTAL = 30
+
+  ;; With special offers
+  (= 30.00
+     (calculate-cart-total [{:id 3
+                             :offer-date "2021-10-01"
+                             :qty 8}
+                            {:id 2
+                             :offer-date "2021-10-01"
+                             :qty 4}]))
+
+
+  ;; Test 2 -------------------
+  ;Cookie, Brownie x 4, Cheesecake. Verify the total is $16.25.
+  ;
+  ;-> 1 Cookie (item code 3) = 1.25
+  ;-> 4 Brownie (item code 1) = 7
+  ;-> 1 Cheesecake (item code 2) = 8
+  ;TOTAL = 16.25
+
+  ;; Without special offers
+  (= 16.25
+     (calculate-cart-total [{:id 3
+                             :qty 1}]))
+
+
+
+  ;; Test 3 -------------------
+  ;Cookie x 8. Verify the total is $8.50
+  ;
+  ;-> 8 Cookie (item code 3) = 8.5
+  ;TOTAL = 8.5
+
+  ;; Without special offers
+  (= 8.5
+     (calculate-cart-total [{:id 3
+                             :qty 8}]))
+
+
+  ;; Test 4 -------------------
+  ;Cookie, Brownie, Cheesecake, Donut x 2. Verify the price is $12.25.
+  ;
+  ;-> 1 Cookie (item code 3) = 1.25
+  ;-> 1 Brownie (item code 1) = 2
+  ;-> 1 Cheesecake (item code 2) = 8
+  ;-> 2 Donut (item code 4) = 1
+  ;TOTAL = 12.25
+
+  ;; Without special offers
+  (= 12.25
+     (calculate-cart-total [{:id 3
+                             :qty 1}
+                            {:id 1
+                             :qty 1}
+                            {:id 2
+                             :qty 1}
+                            {:id 4
+                             :qty 2}]))
+
+
+  :end)
+
+
+
+
+
+
+
 (comment
 
   ;;Test 1 -------------------
@@ -96,12 +178,6 @@
   (get-best-price 2 nil 1)
   (get-best-price 4 nil 2)
 
-
-  ;(offers/get-applicable-offers 1 nil 8)
-  ;(offers/get-applicable-offers 3 "2020-09-18" 8)
-  ;(offers/get-applicable-offers 2 "2020-10-01" 1)
-  ;
-  ;(get-best-price)
   :end)
 
 
